cbfcb274284ce76d78a4b685d2172c51124771ce
juliorriz <102740878+juliorriz@users.noreply.github.com>
2022-03-31 08:30:44 -0500

    Create index.py

---
+++ index.py
@@ -0,0 +1,6 @@
+frutas=["manzana verde","naranja","guanabana","papaya"]
+for i in(frutas)
+    print(i)
+                                                                                                                                                     
+
+                                                                                                                                                     
